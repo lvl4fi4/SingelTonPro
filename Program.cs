4926b3d64edf7b203e1738dd89c9f6ed3c776565
alireza sh <alireza sh>
2016-10-10 09:00:30 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t1 = new Test();
+            Test t2 = new Test();
+            Test t3 = new Test();
+            Test t4 = new Test();
+            Test t5 = new Test();
+
+            SingleTon s1 = SingleTon.getInstance();
+            s1.print("hellllllo");
+            SingleTon s2= SingleTon.getInstance();
+            s2.print("wprld");
+            
+        }
+
+    }
+}
---
+++ SingleTon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    class SingleTon
+    {
+        private static SingleTon Instance = null;
+        private SingleTon() { }
+        public static SingleTon getInstance()
+        {
+            if (Instance == null)
+                Instance = new SingleTon();
+
+            return Instance;
+        }
+        public  void print(string arg)
+        {
+            Console.WriteLine(arg);
+        }
+
+    }
+}
